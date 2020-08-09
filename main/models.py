801db6cb52c20f8300e9a8b622ba19e806ada047
minseon <smsun0329@naver.com>
2020-08-09 15:48:19 +0900

    change_dbmodel_value

--- main/models.py
+++ main/models.py
@@ -4,24 +4,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Store(models.Model):
-    str_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    str_name = models.CharField(max_length=200)
+    str_id = models.AutoField(primary_key=True)
+    str_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    str_name = models.CharField(max_length=50)
     str_email = models.CharField(max_length=100)
-    str_phone = models.CharField(max_length=100)
-    str_address = models.CharField(max_length=300)
-    str_loc_latitude = models.IntegerField(null=True)
-    str_loc_longitude = models.IntegerField(null=True)
+    str_phone = models.CharField(max_length=25)
+    str_address = models.CharField(max_length=200)
+    str_loc_latitude = models.CharField(max_length=50, null=True)
+    str_loc_longitude = models.CharField(max_length=50, null=True)
 
     def __str__(self):
-        return self.str_id
+        return self.str_name
 
 class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
     str_id = models.ForeignKey(Store, on_delete=models.CASCADE)
-    product_id = models.CharField(max_length=100, primary_key=True)
-    product_name = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=100)
     product_image = models.ImageField(upload_to = 'images/')
     product_description = models.CharField(max_length=500)
-    product_default_price = models.IntegerField()
+    product_default_price = models.PositiveIntegerField()
     # detail이 설명에 나올 부분
     product_detail = models.CharField(max_length=100)
     
@@ -59,7 +60,7 @@ class Product(models.Model):
 
 # 테두리, 문구, 문양, 줄무늬,
 # 윗면
-    product_text = models.CharField(max_length=10, default=None)
+    product_text = models.CharField(max_length=10, blank=True)
     product_text_color = models.CharField(max_length=10, choices=COLOR, default=WHITE)
 
     
