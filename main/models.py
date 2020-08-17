8b9871be146aae96af83753796b14719d2b74137
minseon <smsun0329@naver.com>
2020-08-17 21:22:58 +0900

    upload_state_db

--- main/models.py
+++ main/models.py
@@ -75,3 +75,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class Location(models.Model):
+    smallcitycode = models.PositiveIntegerField(unique=True)
+    # loc_id = models.AutoField(primary_key=True, null=False, default='')
+    citycode = models.PositiveIntegerField(blank=True)
+    cityname = models.CharField(max_length=100, blank=True)
+    # smallcitycode = models.PositiveIntegerField(blank=True)
+    smallcityname = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.cityname + " " + self.smallcityname
