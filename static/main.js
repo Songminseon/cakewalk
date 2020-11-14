be22d675797abd1d51ad9a5747e88bb9e6b5ec0b
kimsr96 <62096325+kimsr96@users.noreply.github.com>
2020-11-14 13:17:33 +0900

    responsible

--- static/main.js
+++ static/main.js
@@ -10,6 +10,41 @@ $('.hot_store').slick({
     dots: true,
     prevArrow: false,
     nextArrow: false,
+    mobileFirst:true,
+    responsive: [
+        {
+            breakpoint:1500,
+            settings: {
+                slidesToShow:5
+            }
+        },
+        {
+            breakpoint:1300,
+            settings: {
+                slidesToShow:4
+            }
+        },
+        {
+            breakpoint:1050,
+            settings: {
+                slidesToShow:3
+            }
+        },
+        {
+            breakpoint:720,
+            settings: {
+                slidesToShow:2
+            }
+        },
+        
+        {
+            breakpoint:240,
+            settings: {
+                slidesToShow:1,
+                slidesToScroll:1
+            }
+        }
+    ]
 });
 
 $('.store1_slide').slick({
@@ -31,3 +66,12 @@ const closeModal = () => {
 overlay.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
 openLogin.addEventListener("click",openModal);
+
+const toggleBtn = document.querySelector('.header_toggle');
+const menu = document.querySelector('.header_menu ul');
+const icons = document.querySelector('.header_right');
+
+toggleBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
